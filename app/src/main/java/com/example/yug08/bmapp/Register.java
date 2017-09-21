fac702203235722c508df0f6c6135aa0202e6394
yslee <yug0893@gmail.com>
2017-09-21 22:57:14 +0900

    1st git_cord!! 제발 되라 ...

--- app/src/main/java/com/example/yug08/bmapp/Register.java
+++ app/src/main/java/com/example/yug08/bmapp/Register.java
@@ -1,9 +1,9 @@
 package com.example.yug08.bmapp;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.widget.EditText;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.widget.EditText;
 
 /**
  * Created by yug08 on 2017-09-04.
@@ -11,7 +11,7 @@ import android.widget.EditText;
 
 public class Register extends Activity
 {
-    EditText ET_User_Nmae,ET_Name,ET,_Pass;
+//    EditText ET_User_Nmae,ET_Name,ET,_Pass;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
